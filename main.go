7a9260bed171c19410f3870f3bac61dfd81d92d9
k-engineer-ch <k.engineer.ch2023@gmail.com>
2024-03-19 22:26:34 +0900

    get作成

--- main.go
+++ main.go
@@ -9,11 +9,34 @@ import (
 	_ "github.com/lib/pq"
 )
 
+type Expense struct {
+	ID        int    `json:"id"`
+	Date      string `json:"date"`
+	Amount    string `json:"amount"`
+	Category  string `json:"category"`
+	Memo      string `json:"memo"`
+	CreatedAt string `json:"created_at"`
+}
+
 func main() {
 
 	router := gin.Default()
+
+	// CORSミドルウェアを使用して、CORSポリシーを設定
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusOK)
+			return
+		}
+		c.Next()
+	})
+
 	v1 := router.Group("/v1")
-	v1.GET("/testGet", getting)
+	// v1.GET("/testGet", getting)
+	v1.GET("/getAllExpenses", getAllExpenses)
 	// router.POST("/somePost", posting)
 	// router.PUT("/somePut", putting)
 	// router.DELETE("/someDelete", deleting)
@@ -32,16 +55,50 @@ func getting(c *gin.Context) {
 	db()
 }
 
+func getAllExpenses(c *gin.Context) {
+	// 接続
+	db, err := sql.Open("postgres", "postgres://dev_user:system001@localhost:5432/dev1_db?sslmode=disable")
+	checkErr(err)
+
+	//データの検索
+	rows, err := db.Query("SELECT * FROM expenses")
+	checkErr(err)
+
+	var expenses []Expense
+
+	for rows.Next() {
+		var expense Expense
+		err := rows.Scan(&expense.ID, &expense.Date, &expense.Amount, &expense.Category, &expense.Memo, &expense.CreatedAt)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Internal Server Error",
+			})
+			return
+		}
+		expenses = append(expenses, expense)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+		"data":    expenses,
+	})
+
+	// 接続切る
+	db.Close()
+}
+
 func db() {
 	db, err := sql.Open("postgres", "postgres://dev_user:system001@localhost:5432/dev1_db?sslmode=disable")
 	// db, err := sql.Open("postgres", "postgres://dev_user:system001@hostname:port/dev1_db user=dev_user password=system001 dbname=dev1_db sslmode=disable")
 	checkErr(err)
 
 	//データの挿入
-	stmt, err := db.Prepare("INSERT INTO userinfo(username,departname,created) VALUES($1,$2,$3) RETURNING uid")
+	// stmt, err := db.Prepare("INSERT INTO userinfo(username,departname,created) VALUES($1,$2,$3) RETURNING uid")
+	stmt, err := db.Prepare("INSERT INTO expenses(date,amount,category,memo) VALUES($1,$2,$3,$4) RETURNING id")
 	checkErr(err)
 
-	res, err := stmt.Exec("astaxie", "研究開発部門", "2012-12-09")
+	// res, err := stmt.Exec("astaxie", "研究開発部門", "2012-12-09")
+	res, err := stmt.Exec("2012-12-09", 1000, "food", "テスト登録")
 	checkErr(err)
 	fmt.Println(res)
 
@@ -63,9 +120,24 @@ func db() {
 
 	// fmt.Println(affect)
 
-	// //データの検索
+	//データの検索
 	// rows, err := db.Query("SELECT * FROM userinfo")
-	// checkErr(err)
+	// rows, err := db.Query("SELECT * FROM expenses")
+	// クエリの実行
+	var id int = 2
+	var date string
+	var amount float64
+	var category string
+	var memo string
+	err = db.QueryRow("SELECT id, date, amount, category, memo FROM expenses WHERE id = $1", id).Scan(&id, &date, &amount, &category, &memo)
+	checkErr(err)
+
+	// 結果の表示
+	fmt.Printf("ID: %d\n", id)
+	fmt.Printf("Date: %s\n", date)
+	fmt.Printf("Amount: %f\n", amount)
+	fmt.Printf("Category: %s\n", category)
+	fmt.Printf("Memo: %s\n", memo)
 
 	// for rows.Next() {
 	// 	var uid int
@@ -79,18 +151,34 @@ func db() {
 	// 	fmt.Println(department)
 	// 	fmt.Println(created)
 	// }
+	// for rows.Next() {
+	// 	var id int
+	// 	var date string
+	// 	var amount string
+	// 	var category string
+	// 	var memo string
+	// 	var created_at string
+	// 	err = rows.Scan(&id, &date, &amount, &category, &memo, &created_at)
+	// 	checkErr(err)
+	// 	fmt.Println(id)
+	// 	fmt.Println(date)
+	// 	fmt.Println(amount)
+	// 	fmt.Println(category)
+	// 	fmt.Println(memo)
+	// 	fmt.Println(created_at)
+	// }
 
-	// //データの削除
-	// stmt, err = db.Prepare("delete from userinfo where uid=$1")
-	// checkErr(err)
+	//データの削除
+	stmt, err = db.Prepare("delete from expenses where id=$1")
+	checkErr(err)
 
-	// res, err = stmt.Exec(1)
-	// checkErr(err)
+	res, err = stmt.Exec(2)
+	checkErr(err)
 
-	// affect, err = res.RowsAffected()
-	// checkErr(err)
+	affect, err := res.RowsAffected()
+	checkErr(err)
 
-	// fmt.Println(affect)
+	fmt.Println(affect)
 
 	db.Close()
 }
